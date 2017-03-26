4b3cbb68f2c576e39d24417cb99748489eeccf0e
Alex Ling <hkalexling@gmail.com>
2017-03-26 18:47:30 +0800

    GitHub

---
+++ Podfile
@@ -0,0 +1,5 @@
+use_frameworks!
+platform :osx, '10.10'
+target 'ProgressWall' do
+	pod 'ReactiveObjC'
+end
--- ProgressWall/AppDelegate.m
+++ ProgressWall/AppDelegate.m
@@ -7,21 +7,193 @@
 //
 
 #import "AppDelegate.h"
+#import <ReactiveObjC/ReactiveObjC.h>
+
+#define TICK NSDate *startTime = [NSDate date]
+#define TOCK NSLog(@"Time: %f", -[startTime timeIntervalSinceNow])
+
+#define kScreenWidth 2880.0
+#define kScreenHeight 1800.0
+#define kTextWidth kScreenWidth * 0.8
 
 @interface AppDelegate ()
 
 @end
 
-@implementation AppDelegate
+@implementation AppDelegate{
+	NSStatusItem *item;
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-	// Insert code here to initialize your application
+	
+	item = [[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength];
+	item.button.image = nil;
+	
+	NSMenu *menu = [NSMenu new];
+	[menu addItem:[[NSMenuItem alloc] initWithTitle:@"Quit" action:@selector(quit) keyEquivalent:@"q"]];
+	
+	item.menu = menu;
+	
+	[[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self selector:@selector(workspaceChanged:) name:NSWorkspaceActiveSpaceDidChangeNotification object:[NSWorkspace sharedWorkspace]];
+	
+	[self workspaceChanged:nil];
+	
+	[[[[self imageWithText:[self progressString]]
+		 flattenMap:^__kindof RACSignal * _Nullable(NSImage *img) {
+		return [self saveImageToSupport:img];
+	}] subscribeOn:[RACScheduler mainThreadScheduler]]
+	 subscribeNext:^(NSString *path) {
+		NSLog(@"path: %@", path);
+	} error:^(NSError * _Nullable error) {
+		NSLog(@"error: %@", error);
+	} completed:^{
+		NSLog(@"completed");
+	}];
 }
 
-
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-	// Insert code here to tear down your application
+- (void)quit {
+	exit(0);
 }
 
+- (void)workspaceChanged: (NSNotification *)notification{
+	
+}
+
+#pragma mark - Utility
+
+- (RACSignal *)imageWithText: (NSString *) text {
+	return [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+		
+		NSImage *img = [[NSImage alloc] initWithSize:NSMakeSize(kScreenWidth, kScreenHeight)];
+		NSDictionary *attr = @{
+													 NSFontAttributeName: [NSFont fontWithName:@"Avenir" size:120 ],
+													 NSForegroundColorAttributeName: [NSColor whiteColor]
+													 };
+		NSAttributedString *attrStr = [[NSAttributedString alloc] initWithString:text attributes:attr];
+		CGRect boundRect = [attrStr boundingRectWithSize:CGSizeMake(kTextWidth, CGFLOAT_MAX) options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading];
+		
+		[img lockFocus];
+		
+		[[NSColor blackColor] drawSwatchInRect:NSMakeRect(0, 0, kScreenWidth, kScreenHeight)];
+		
+		
+		NSRect rect = NSMakeRect((kScreenWidth - boundRect.size.width)/2, (kScreenHeight - boundRect.size.height)/2, boundRect.size.width, boundRect.size.height);
+		[text drawInRect:rect withAttributes:attr];
+		
+		[img unlockFocus];
+		
+		[subscriber sendNext:img];
+		[subscriber sendCompleted];
+		
+		return nil;
+	}];
+}
+
+- (NSString *) supportPath {
+	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
+	NSString *support = [[paths firstObject] stringByAppendingPathComponent:@"com.hkalexling.progress-wall"];
+	return support;
+}
+
+- (RACSignal *) clearSupportDirectory {
+	return [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+			NSFileManager *manager = [NSFileManager defaultManager];
+			NSDirectoryEnumerator *enumerator = [manager enumeratorAtPath:[self supportPath]];
+			NSError *error;
+			
+			NSString *file;
+			while (file = [enumerator nextObject]) {
+				if ([[file pathExtension] isEqualToString:@"jpg"]) {
+					[manager removeItemAtPath:[[self supportPath] stringByAppendingPathComponent:file] error:&error];
+				}
+			}
+			
+			dispatch_async(dispatch_get_main_queue(), ^{
+				if (error){
+					[subscriber sendError:error];
+				}
+				else{
+					[subscriber sendCompleted];
+				}
+			});
+		});
+		
+		return nil;
+	}];
+}
+
+- (RACSignal *)saveImageToSupport:(NSImage *)image {
+	return [[self clearSupportDirectory] then:^RACSignal * _Nonnull{
+		return [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+			NSFileManager *manager = [NSFileManager defaultManager];
+			
+			dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+				BOOL isDir;
+				if (![manager fileExistsAtPath:[self supportPath] isDirectory:&isDir]){
+					NSError *error;
+					[manager createDirectoryAtPath:[self supportPath] withIntermediateDirectories:YES attributes:nil error:&error];
+					if (error){
+						dispatch_async(dispatch_get_main_queue(), ^{
+							[subscriber sendError:error];
+						});
+					}
+				}
+				
+				NSString *name = [NSString stringWithFormat:@"%@.jpg", @([[NSDate date] timeIntervalSince1970])];
+				NSString *imgPath = [[self supportPath] stringByAppendingPathComponent:name];
+				
+				NSData *imageData = [image TIFFRepresentation];
+				NSBitmapImageRep *rep = [NSBitmapImageRep imageRepWithData:imageData];
+				NSDictionary *imageProps = [NSDictionary dictionaryWithObject:[NSNumber numberWithFloat:1.0] forKey:NSImageCompressionFactor];
+				imageData = [rep representationUsingType:NSJPEGFileType properties:imageProps];
+				[imageData writeToFile:imgPath atomically:YES];
+				
+				dispatch_async(dispatch_get_main_queue(), ^{
+					[subscriber sendNext:imgPath];
+					[subscriber sendCompleted];
+				});
+			});
+			
+			return nil;
+		}];
+	}];
+}
+
+#pragma mark - Date Utility
+
+- (NSDate *)firstDateOfYear:(NSInteger)year
+{
+	NSDateComponents *dc = [[NSDateComponents alloc] init];
+	dc.year = year;
+	dc.month = 1;
+	dc.day = 1;
+	return [[NSCalendar currentCalendar] dateFromComponents:dc];
+}
+
+- (NSInteger)numberOfDaysInYear{
+	NSCalendar *cal = [NSCalendar currentCalendar];
+	NSInteger year = [cal component:NSCalendarUnitYear fromDate:[NSDate date]];
+	
+	NSDate *firstDateOfYear = [self firstDateOfYear:year];
+	NSDate *firstDateOfNextYear = [self firstDateOfYear:year + 1];
+	
+	NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitDay fromDate:firstDateOfYear toDate:firstDateOfNextYear options:0];
+	return [components day];
+}
+
+- (NSInteger)dayInYear{
+	NSCalendar *cal = [NSCalendar currentCalendar];
+	NSInteger dayInYear = [cal ordinalityOfUnit:NSCalendarUnitDay inUnit:NSCalendarUnitYear forDate:[NSDate date]];
+	return dayInYear;
+}
+
+- (CGFloat)progress {
+	return (CGFloat)[self dayInYear]/(CGFloat)[self numberOfDaysInYear];
+}
+
+- (NSString *)progressString {
+	return [NSString stringWithFormat:@"%.0f%%", [self progress] * 100];
+}
 
 @end
